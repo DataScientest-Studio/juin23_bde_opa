b1cadaa84f836ffd56d0e550fb02d0c229b7d45f
jherve <julien.jev.herve@gmail.com>
2023-07-16 18:36:20 +0200

    Add dev requirements file

--- setup-local.sh
+++ setup-local.sh
@@ -6,4 +6,5 @@ python -m venv ${LOCAL_VENV}
 source ${LOCAL_VENV}/bin/activate
 
 pip install --upgrade pip
+pip install -r requirements-dev.txt
 pip install -r requirements-financial_data_reader.txt
