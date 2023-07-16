07ad390e5eebf89c5c012f2674acfbcda15da891
jherve <julien.jev.herve@gmail.com>
2023-07-16 21:45:34 +0200

    Run data_report code only as __main__ and set HOST in docker-compose

--- data_report.py
+++ data_report.py
@@ -6,9 +6,6 @@ import pandas as pd
 from storage import get_historical
 
 
-dash_app = Dash(__name__)
-
-
 def get_dataframe(ticker):
     data = get_historical(ticker)
     return pd.DataFrame(data)
@@ -19,14 +16,17 @@ def update_graph(ticker):
     return px.line(get_dataframe(ticker), x="date", y="close")
 
 
-dash_app.layout = html.Div(
-    [
-        html.Div(children="My First App with Data"),
-        dcc.Dropdown(
-            ["AAPL", "MSFT", "AMZN", "GOOG", "META"], "AAPL", id="dropdown-selection"
-        ),
-        dcc.Graph(id="graph-content"),
-    ]
-)
-
-dash_app.run(host="0.0.0.0", debug=False)
+if __name__ == "__main__":
+    dash_app = Dash(__name__)
+    dash_app.layout = html.Div(
+        [
+            html.Div(children="My First App with Data"),
+            dcc.Dropdown(
+                ["AAPL", "MSFT", "AMZN", "GOOG", "META"],
+                "AAPL",
+                id="dropdown-selection",
+            ),
+            dcc.Graph(id="graph-content"),
+        ]
+    )
+    dash_app.run(debug=False)
