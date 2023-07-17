91d94cb2ff4bfaa9268fb53db2ad9df3db58b62e
jherve <julien.jev.herve@gmail.com>
2023-07-17 11:50:36 +0200

    Change StockMarketProvider interface to handle streaming data

--- opa/providers.py
+++ opa/providers.py
@@ -1,6 +1,6 @@
 import os
 from abc import ABC, abstractmethod
-from datetime import date
+from datetime import date, datetime
 
 from pydantic import BaseModel
 
@@ -12,12 +12,30 @@ class StockMarketProvider(ABC):
     def __init__(self):
         self.access_key = get_secret(self.api_key_secret_file)
 
-    @abstractmethod
     def get_historical_data(self, ticker):
+        raw = self.get_raw_historical_data(ticker)
+        return self.into_historical_data(raw)
+
+    def get_streaming_data(self, ticker):
+        raw = self.get_raw_streaming_data(ticker)
+        return self.into_streaming_data(raw)
+
+    @abstractmethod
+    def get_raw_historical_data(self, ticker):
+        ...
+
+    @staticmethod
+    @abstractmethod
+    def into_historical_data(data):
         ...
 
     @abstractmethod
-    def get_streaming_data(self, ticker):
+    def get_raw_streaming_data(self, ticker):
+        ...
+
+    @staticmethod
+    @abstractmethod
+    def into_streaming_data(data):
         ...
 
 
@@ -31,18 +49,35 @@ class FmpCloudHistoricalData(BaseModel):
     historical: list[FmpCloudHistoricalValue]
 
 
+class FmpCloudStreamingValue(BaseModel):
+    date: datetime
+    open: float
+    close: float
+    low: float
+    high: float
+    volume: int
+
+
 class FmpCloud(StockMarketProvider):
     api_key_secret_file = "fmp_cloud_api_key"
 
-    def get_historical_data(self, ticker):
-        json = get_json_data(
+    def get_raw_historical_data(self, ticker):
+        return get_json_data(
             f"https://fmpcloud.io/api/v3/historical-price-full/{ticker}",
             params={"serietype": "line", "apikey": self.access_key},
         )
-        return FmpCloudHistoricalData(**json)
 
-    def get_streaming_data(self, ticker):
+    @staticmethod
+    def into_historical_data(data):
+        return FmpCloudHistoricalData(**data)
+
+
+    def get_raw_streaming_data(self, ticker):
         return get_json_data(
             f"https://fmpcloud.io/api/v3/historical-chart/15min/{ticker}",
             params={"apikey": self.access_key},
         )
+
+    @staticmethod
+    def into_streaming_data(data):
+        return [FmpCloudStreamingValue(**v) for v in data]
