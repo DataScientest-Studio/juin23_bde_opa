e088ac3e26563e75d5cd0f9f5c6b7c472b989553
jherve <julien.jev.herve@gmail.com>
2023-07-23 16:59:56 +0200

    Add /tickers and /{ticker}/historical endpoints

--- opa/api.py
+++ opa/api.py
@@ -10,3 +10,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/tickers")
+async def all_tickers():
+    return storage.get_all_tickers()
+
+
+@app.get("/{ticker}/historical")
+async def historical(ticker: str):
+    return storage.get_historical(ticker)
