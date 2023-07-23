fbe8a29c683f55a65b98894457cf31ff2ada33aa
jherve <julien.jev.herve@gmail.com>
2023-07-23 18:16:03 +0200

    Move reader main operation into a function

--- opa/financial_data_reader.py
+++ opa/financial_data_reader.py
@@ -1,16 +1,25 @@
-from opa.providers import FmpCloud
+from opa.financial_data import StockValue
+from opa.providers import StockMarketProvider, FmpCloud
 from opa.storage import storage
 
 
 ALL_VALUES = ["AAPL", "MSFT", "AMZN", "GOOG", "META"]
 
 
+def retrieve_data_and_store(
+    provider: StockMarketProvider, tickers: list[str]
+) -> list[StockValue]:
+    values = [
+        stock_value
+        for ticker in tickers
+        for stock_value in provider.get_historical_data(ticker)
+    ]
+    storage.insert_historical(values)
+    return values
+
+
 if __name__ == "__main__":
     print("Hello from financial_data_reader")
 
     fmp = FmpCloud()
-
-    historical = {ticker: fmp.get_historical_data(ticker) for ticker in ALL_VALUES}
-
-    all_values = [v for vals in historical.values() for v in vals]
-    storage.insert_historical(all_values)
+    retrieve_data_and_store(fmp, ALL_VALUES)
