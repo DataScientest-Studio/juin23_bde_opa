08bf502ee5e61564c58302a00b05cb1d57ea75b7
jherve <julien.jev.herve@gmail.com>
2023-07-26 16:23:55 +0200

    Local and containerized http caches now use the same local database

--- opa/http_methods.py
+++ opa/http_methods.py
@@ -1,10 +1,22 @@
 import os
+from pathlib import Path
 import requests
+
 import requests_cache
+from requests_cache.backends import SQLiteCache
+
+from opa.core.env import is_running_in_docker
 
 
+http_cache_db_dir = (
+    Path("/var/http_cache")
+    if is_running_in_docker()
+    else Path.cwd() / "app_data" / "http_cache"
+)
 session = (
-    requests_cache.CachedSession("opa")
+    requests_cache.CachedSession(
+        "opa", backend=SQLiteCache(db_path=http_cache_db_dir / "opa")
+    )
     if os.getenv("USE_HTTP_CACHE", False)
     else requests.Session()
 )
