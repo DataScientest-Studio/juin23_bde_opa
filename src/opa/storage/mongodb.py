ae3bdaa4160bd0b89b24030e93271b4e10f65b89
jherve <julien.jev.herve@gmail.com>
2023-08-07 19:38:40 +0200

    Better error messages in case of error on insert
    This is related to issue #3 but maybe not the optimal solution.

--- src/opa/storage/mongodb.py
+++ src/opa/storage/mongodb.py
@@ -1,9 +1,18 @@
+from collections import Counter
+
 from pymongo import MongoClient
 from pymongo.errors import BulkWriteError
 from loguru import logger
 
 from opa.core.financial_data import StockValue, StockValueType, CompanyInfo
 from opa.core.storage import Storage
+
+
+# MongoDB error codes that we handle here
+# https://github.com/mongodb/mongo/blob/master/src/mongo/base/error_codes.yml
+MULTIPLE_ERRORS_OCCURRED_ERROR = 65
+DUPLICATE_KEY_ERROR = 11000
+DOCUMENT_VALIDATION_FAILURE_ERROR = 121
 
 
 class MongoDbStorage(Storage):
@@ -35,21 +44,69 @@ class MongoDbStorage(Storage):
 
             return ret
         except BulkWriteError as err:
-            error_codes = {e["code"] for e in err.details["writeErrors"]}
-            write_errors = err.details["writeErrors"]
-            error_codes = {e["code"] for e in write_errors}
-            duplicate_keys = {
-                frozenset(e["keyPattern"].keys())
-                for e in write_errors
-                if e["code"] == 11000
-            }
+            # BulkWriteError has a structure that is not very well documented either in
+            # pymongo or in MongoDB documentation :
+            #   * an error code `code`
+            #   * details about the errors in `details`
+            #
+            # `err.details`` is a dictionary with the following keys :
+            #
+            # ```
+            # dict_keys([
+            #    'writeErrors', 'writeConcernErrors', 'nInserted', 'nUpserted', 'nMatched', 'nModified', 'nRemoved', 'upserted'
+            # ])
+            # ```
+            #
+            # `err.details["writeErrors"]` is a [verbose] list of ALL the errors.
+            #
+            # Errors have the following structure :
+            # {'index': * the index of the document that failed validation within the stream we sent * ,
+            #  'code': * the error code *,
+            #  'errmsg': * a nice error message *,
+            #  'op': * the document we tried to insert, as a dict *}
+            #
+            # Duplication errors have the following additional fields :
+            # {'keyPattern': * the fields that are duplicated, as a dict *,
+            #  'keyValue': * the value of the fields that are duplicates, as a dict *}
+            #
+            # Validation errors have the following additional fields :
+            # {'errInfo': * a dictionary with additional details *}
+            # The errInfo["details"] dictionary has the keys "operatorName", "title", "schemaRulesNotSatisfied".
+            if err.code != MULTIPLE_ERRORS_OCCURRED_ERROR:
+                raise err
 
-            if error_codes < {121, 11000} and duplicate_keys == {
-                frozenset(["ticker", "date"])
-            }:
-                logger.warning(
-                    "All the new stock values failed validation or were duplicates"
+            write_errors = err.details["writeErrors"]
+
+            nb_inserted = err.details["nInserted"]
+            code_counter = Counter((e["code"] for e in write_errors))
+            duplicate_counter = Counter(
+                (
+                    frozenset(e["keyPattern"].keys())
+                    for e in write_errors
+                    if e["code"] == DUPLICATE_KEY_ERROR
                 )
+            )
+
+            logger.warning(
+                "Only {nb_inserted} stock values out of {write_operations} could be inserted "
+                "({nb_expected_duplicates} were duplicates of existing values)",
+                nb_inserted=nb_inserted,
+                write_operations=len(insertable),
+                nb_expected_duplicates=duplicate_counter[frozenset(["ticker", "date"])],
+            )
+
+            unexpected_duplicates = (
+                code_counter[DUPLICATE_KEY_ERROR]
+                - duplicate_counter[frozenset(["ticker", "date"])]
+            )
+            if unexpected_duplicates:
+                logger.error(
+                    "{} values were unexpected duplicates", unexpected_duplicates
+                )
+
+            validation_errors = code_counter[DOCUMENT_VALIDATION_FAILURE_ERROR]
+            if validation_errors:
+                logger.error("{} documents had validation errors", validation_errors)
 
     def get_values(
         self, ticker: str, type_: StockValueType, limit: int = 500
