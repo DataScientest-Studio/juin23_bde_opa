7270f7957f4549bd95cd96fa2567f04f4b6d26fb
jherve <julien.jev.herve@gmail.com>
2023-08-11 20:22:17 +0200

    Add a test for stock values retrieval

--- tests/integration/test_storage.py
+++ tests/integration/test_storage.py
@@ -1,15 +1,15 @@
 import pytest
-import os
 from faker import Faker
+from datetime import timedelta
 
-from opa.core import CompanyInfo
+from opa.core import CompanyInfo, StockValue, StockValueType
 from opa.storage import opa_storage
 
 
 fake = Faker()
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="function", autouse=True)
 def db_wipeout():
     # This relies on knowledge of opa_storage implementation but it's good enough
     # in this scope
@@ -19,6 +19,35 @@ def db_wipeout():
         c.delete_many({})
 
     yield
+
+
+@pytest.fixture
+def ticker():
+    return fake.pystr(max_chars=5).upper()
+
+
+@pytest.fixture(params=StockValueType)
+def stock_value_type(request):
+    return request.param
+
+
+@pytest.fixture
+def stock_values_serie(stock_value_type, ticker) -> list[StockValue]:
+    start = fake.date_time()
+
+    if stock_value_type == StockValueType.HISTORICAL:
+        dates = [start + timedelta(days=n) for n in range(100)]
+    elif stock_value_type == StockValueType.STREAMING:
+        dates = [start + timedelta(minutes=15 * n) for n in range(100)]
+
+    return [
+        StockValue(
+            ticker=ticker,
+            date=d,
+            close=fake.pyfloat(right_digits=2, min_value=0.5, max_value=100.0),
+        )
+        for d in dates
+    ]
 
 
 @pytest.fixture
@@ -42,7 +71,16 @@ def company_infos() -> list[CompanyInfo]:
 
 
 class TestIntegration:
-    def test_company_info_retrieval(self, company_infos, db_wipeout):
+    def test_values_retrieval(self, ticker, stock_values_serie, stock_value_type):
+        """`get_values` should return all the values that were inserted via `insert_values`,
+        sorted by most-recent first"""
+        opa_storage.insert_values(stock_values_serie, stock_value_type)
+
+        expected = sorted(stock_values_serie, key=lambda v: v.date, reverse=True)
+
+        assert opa_storage.get_values(ticker, stock_value_type) == expected
+
+    def test_company_info_retrieval(self, company_infos):
         opa_storage.insert_company_infos(company_infos)
 
         expected = {c.symbol: c for c in company_infos}
