4cd97f7145b089ee93d55cb8714f27a5b6d9f533
jherve <julien.jev.herve@gmail.com>
2023-08-14 12:25:08 +0200

    Mock objects' method directly in fixture

--- tests/unit/test_reader.py
+++ tests/unit/test_reader.py
@@ -6,13 +6,30 @@ from opa.core import FinancialDataReader, Storage, StockMarketProvider
 @pytest.fixture
 def provider(mocker):
     mocker.patch.multiple(StockMarketProvider, __abstractmethods__=set())
-    return StockMarketProvider()
+    provider = StockMarketProvider()
+
+    for method in ["get_stock_values", "get_company_info"]:
+        mocker.patch.object(provider, method, autospec=True)
+
+    return provider
 
 
 @pytest.fixture
 def storage(mocker):
     mocker.patch.multiple(Storage, __abstractmethods__=set())
-    return Storage()
+    storage = Storage()
+
+    for method in [
+        "insert_values",
+        "get_values",
+        "get_all_tickers",
+        "insert_company_infos",
+        "get_company_infos",
+        "get_stats",
+    ]:
+        mocker.patch.object(storage, method, autospec=True)
+
+    return storage
 
 
 @pytest.fixture
@@ -20,16 +37,17 @@ def reader(provider, storage):
     return FinancialDataReader(provider, storage)
 
 
-class TestReader:
-    def test_import_company_info(
-        self, mocker, provider, storage, reader, company_infos
-    ):
-        mocker.patch.object(provider, "get_company_info", autospec=True)
+class TestImportCompanyInfo:
+    @pytest.fixture
+    def provider_for_company_infos(self, provider, company_infos):
         provider.get_company_info.return_value = company_infos
-        mocker.patch.object(storage, "insert_company_infos", autospec=True)
+        return provider
 
+    def test_import_company_info(
+        self, provider_for_company_infos, storage, reader, company_infos
+    ):
         tickers = [c.symbol for c in company_infos]
         reader.import_company_info(tickers)
 
-        provider.get_company_info.assert_called_once_with(tickers)
+        provider_for_company_infos.get_company_info.assert_called_once_with(tickers)
         storage.insert_company_infos.assert_called_once_with(company_infos)
