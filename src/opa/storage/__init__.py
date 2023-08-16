2f10bb02582af18b43b3efa32f379a3570298d46
jherve <julien.jev.herve@gmail.com>
2023-08-16 19:52:08 +0200

    Change exposed mongodb port and add --auth
    Related to issue #7
    See https://dev.to/ndaidong/how-to-make-your-mongodb-container-more-secure-1646

--- src/opa/storage/__init__.py
+++ src/opa/storage/__init__.py
@@ -5,10 +5,11 @@ from opa import settings
 from .mongodb import MongoDbStorage
 
 
-mongodb_uri = "mongodb://{username}:{password}@{host}".format(
+mongodb_uri = "mongodb://{username}:{password}@{host}:{port}".format(
     username=settings.secrets.mongodb_username,
     password=settings.secrets.mongodb_password,
     host=settings.mongo_host,
+    port=settings.mongo_port,
 )
 
 opa_storage = MongoDbStorage(mongodb_uri, settings.mongo_database)
