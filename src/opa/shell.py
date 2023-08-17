b3f5eb20e3b1a34b03138a90187982f308a37e4e
jherve <julien.jev.herve@gmail.com>
2023-08-17 10:20:22 +0200

    Improve interactive shell with autoreload extension

--- src/opa/shell.py
+++ src/opa/shell.py
@@ -2,11 +2,21 @@
 A simple interactive shell based on IPython that can be used for dev/debugging purposes
 """
 
-from IPython import embed
+from IPython import start_ipython
+from traitlets.config import Config
 
-from opa.core import *
-from opa.http_methods import get_json_data
-from opa.providers import opa_provider
-from opa.storage import opa_storage
 
-embed()
+c = Config()
+c.InteractiveShellApp.extensions = ["autoreload"]
+c.InteractiveShellApp.exec_lines = [
+    'print("Welcome to OPA Shell")',
+    "%autoreload 2",
+    "from opa.core import *",
+    "from opa.http_methods import get_json_data",
+    "from opa.providers import opa_provider",
+    "from opa.storage import opa_storage",
+]
+c.InteractiveShell.confirm_exit = False
+c.TerminalIPythonApp.display_banner = False
+
+start_ipython(config=c)
