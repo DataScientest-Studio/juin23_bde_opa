fda02ba147ffede2d66ca10367f563fafd899168
jherve <julien.jev.herve@gmail.com>
2023-08-21 10:48:37 +0200

    Wrap all javascript in lit element

---
+++ src/opa/vega.js
@@ -0,0 +1,82 @@
+import { html, css, LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
+export class VegaGraph extends LitElement {
+    static properties = {
+        _kind: { type: String, state: true },
+        _ticker: { type: String, state: true },
+        _tickers: { state: true },
+        _kinds: { state: true },
+        _dataSetName: { state: true },
+        _view: { state: true }
+    };
+
+    constructor() {
+        super();
+
+        this._tickers = ["AAPL", "MSFT", "META"];
+        this._ticker = this._tickers[0];
+
+        this._kinds = ["ohlc", "simple"];
+        this._kind = this._kinds[0];
+
+        this.replaceGraph();
+    }
+
+    dataUrl() {
+        return `/json/${this._ticker}?kind=${this._kind}`
+    }
+
+    replaceGraph() {
+        var self = this;
+        fetch(this.dataUrl()).then(resp => {
+            resp.json().then(json => {
+                vegaEmbed(this.renderRoot.querySelector('#vega'), json).then(function (result) {
+                    self._view = result.view
+                    self._dataSetName = json.data.name
+                }).catch(console.error);
+            }).catch(console.error);
+        });
+    }
+
+    updateGraph() {
+        var self = this;
+        fetch(this.dataUrl()).then(resp => {
+            resp.json().then(json => {
+                var changeSet = vega.changeset().remove(() => true).insert(json.datasets[json.data.name]);
+                self._view.change(this._dataSetName, changeSet).run();
+            })
+        });
+    }
+
+    _changeTicker(e) {
+        this._ticker = e.target.value;
+        this.updateGraph();
+    }
+
+    _changeKind(e) {
+        this._kind = e.target.value;
+        this.replaceGraph();
+    }
+
+    render() {
+        return html`
+      <label for="select-ticker">Choose a ticker:</label>
+  
+      <select name="tickers" id="select-ticker" @change="${this._changeTicker}">
+        ${this._tickers.map((t) =>
+            html`<option value="${t}" ?selected=${this._ticker == t}>${t}</option>`
+        )}
+      </select>
+  
+      <label for="select-kind">Choose a data kind:</label>
+  
+      <select name="kinds" id="select-kind" @change="${this._changeKind}">
+        ${this._kinds.map((k) =>
+            html`<option value="${k}" ?selected=${this._kind == k}>${k}</option>`
+        )}
+      </select>
+
+      <div id="vega"></div>
+      `;
+    }
+}
+customElements.define('vega-graph', VegaGraph);
--- src/opa/vega.py
+++ src/opa/vega.py
@@ -93,115 +93,8 @@ async def stock_graph(ticker: str, kind: StockValueKind = StockValueKind.OHLC):
     graph = get_graph(ticker, kind)
     tickers = opa_storage.get_all_tickers()
 
-    def to_ticker_url(ticker, kind=kind):
-        return f"/json/{ticker}?kind={kind.value}"
-
-    script = """
-        function replaceVegaGraph(vegaView, name, dataUrl) {
-            fetch(dataUrl).then(resp => {
-                resp.json().then(json => {
-                    delete window.view;
-                    vegaEmbed('#vis', json).then(function(result) {
-                        console.log("updated")
-                        window.view = result.view
-                    }).catch(console.error);
-                })
-            })
-        }
-
-        function updateVegaData(vegaView, name, dataUrl) {
-            fetch(dataUrl).then(resp => {
-                resp.json().then(json => {
-                    var changeSet = vega.changeset().remove(() => true).insert(json.datasets[json.data.name]);
-                    vegaView.change(name, changeSet).run();
-                })
-            })
-        }
-
-        function getSelectedValues() {
-            return new Map(
-                Array.from(document.getElementsByTagName("select"),
-                (e) => [e.name, e.selectedOptions[0].value]
-                )
-            )
-        }
-
-        function getNewUrl() {
-            const values = getSelectedValues()
-            return `/json/${values.get("tickers")}?kind=${values.get("kinds")}`
-        }
-
-        fetch("/json/%(ticker)s?kind=%(kind)s").then(resp => {
-            resp.json().then(json => {
-                vegaEmbed('#vis', json).then(function(result) {
-                    window.view = result.view
-                    var currentName = json.data.name
-                    var selects = Array.from(document.getElementsByTagName("select"));
-                    selects.forEach((s) => {
-                        s.addEventListener("change", (e) => {
-                            replaceVegaGraph(result.view, currentName, getNewUrl());
-                        });
-                    })
-                }).catch(console.error);
-            });
-        });
-    """ % {
-        "ticker": ticker,
-        "kind": kind.value,
-    }
-
-    lit_script = """
-import {html, css, LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
-
-export class VegaGraph extends LitElement {
-    static properties = {
-      _kind: {type: String, state: true},
-      _ticker: {type: String, state: true},
-      _tickers: {state: true},
-      _kinds: {state: true},
-    };
-
-    constructor() {
-      super();
-      this._kind = "ohlc";
-      this._ticker = "AAPL";
-      this._tickers = ["AAPL", "MSFT", "META"];
-      this._kinds = ["ohlc", "simple"];
-    }
-
-    _changeTicker(e) {
-      this._ticker = e.target.value;
-    }
-
-    _changeKind(e) {
-      this._kind = e.target.value;
-    }
-
-    render() {
-      return html`
-      <p>current > ${this._ticker} <, > ${this._kind} <</p>
-
-      <label for="ticker-select">Choose a ticker:</label>
-
-      <select name="tickers" id="select-ticker" @change="${this._changeTicker}">
-        ${this._tickers.map((t) =>
-            html`<option value="${t}" ?selected=${this._ticker == t}>${t}</option>`
-        )}
-      </select>
-
-      <label for="kind-select">Choose a data kind:</label>
-
-      <select name="kinds" id="select-kind" @change="${this._changeKind}">
-        ${this._kinds.map((k) =>
-            html`<option value="${k}" ?selected=${this._kind == k}>${k}</option>`
-        )}
-      </select>
-      `;
-    }
-}
-customElements.define('vega-graph', VegaGraph);
-"""
-
+    with open("src/opa/vega.js") as f:
+        lit_script = f.read()
     return HTMLResponse(
         content=f"""
 
@@ -215,27 +108,8 @@ customElements.define('vega-graph', VegaGraph);
   <script src="https://cdn.jsdelivr.net/npm/vega-embed@5/build/vega-embed.js"></script>
 </head>
 <body>
-<vega-graph/>
-<div id="vis"></div>
-<label for="ticker-select">Choose a ticker:</label>
+<vega-graph></vega-graph>
 
-<select name="tickers" id="ticker-select">
-  <option value="AAPL">AAPL</option>
-  <option value="MSFT">MSFT</option>
-  <option value="META">META</option>
-</select>
-
-<label for="kind-select">Choose a data kind:</label>
-<select name="kinds" id="kind-select">
-  <option value="{StockValueKind.OHLC.value}">OHLC</option>
-  <option value="{StockValueKind.SIMPLE.value}">simple</option>
-</select>
-
-<script type="text/javascript">
-  {script}
-</script>
-<script type="text/javascript">
-</script>
 <script type="module">
     {lit_script}
 </script>
