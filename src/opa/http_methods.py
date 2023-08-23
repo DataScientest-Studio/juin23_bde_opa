c9afb72c73c18088d3911ab9c79b4144194e1760
jherve <julien.jev.herve@gmail.com>
2023-08-23 10:29:35 +0200

    Rework http_methods module to work when requests_cache is not available

--- src/opa/http_methods.py
+++ src/opa/http_methods.py
@@ -1,27 +1,39 @@
-import requests
 from datetime import timedelta
 from pathlib import Path
 
 from loguru import logger
-import requests_cache
-from requests_cache.models.response import CachedResponse
-from requests_cache.backends.sqlite import SQLiteCache
 
 from opa import settings
 
 
-session = (
-    requests_cache.CachedSession(
-        "opa",
-        backend=SQLiteCache(db_path=Path(settings.http_cache_dir) / "opa"),
-        # This should certainly be set by request, but within the limited scope of this
-        # project, new data is fetched every day, and the API rate is limited anyway.
-        # Hardcoding the value is good enough.
-        expire_after=timedelta(hours=12.0),
-    )
-    if settings.use_http_cache
-    else requests.Session()
-)
+def get_session():
+    if settings.use_http_cache:
+        import requests_cache
+        from requests_cache.models.response import CachedResponse
+        from requests_cache.backends.sqlite import SQLiteCache
+
+        return requests_cache.CachedSession(
+            "opa",
+            backend=SQLiteCache(db_path=Path(settings.http_cache_dir) / "opa"),
+            # This should certainly be set by request, but within the limited scope of this
+            # project, new data is fetched every day, and the API rate is limited anyway.
+            # Hardcoding the value is good enough.
+            expire_after=timedelta(hours=12.0),
+        )
+    else:
+        import requests
+
+        return requests.Session()
+
+
+def is_cached(request):
+    try:
+        return request.from_cache
+    except AttributeError:
+        return False
+
+
+session = get_session()
 
 
 def get_json_data(url: str, **kwargs):
@@ -33,7 +45,7 @@ def get_json_data(url: str, **kwargs):
 
     logger.debug(
         "Got successful HTTP response from {cached}{url}",
-        cached="cached " if isinstance(http, CachedResponse) else "",
+        cached="cached " if is_cached(http) else "",
         url=http.url,
     )
 
