99b78132d56cbb8f0710c8e2172171f099e80bb2
jherve <julien.jev.herve@gmail.com>
2023-09-12 19:01:17 +0200

    A better split

--- src/opa/auth.py
+++ src/opa/auth.py
@@ -1,22 +1,22 @@
 import json
 import base64
 
-from opa.core.auth import Authenticator
+from opa.core.auth import Authenticator, CredentialsStorage
 
 
-class JsonCredentialsAuthenticator(Authenticator):
+class JsonCredentialsStorage(CredentialsStorage):
     def __init__(self, file) -> None:
         self.creds_file = file
 
     def read_hashed_password(self, username: str) -> bytes | None:
         return self._read_credentials_file().get(username)
 
-    def write_credentials(self, username: str, hashed_password: bytes):
+    def write(self, username: str, hashed_password: bytes):
         credentials = self._read_credentials_file()
         credentials[username] = hashed_password
         self._write_credentials_file(credentials)
 
-    def remove_credentials(self, username: str):
+    def remove(self, username: str):
         credentials = self._read_credentials_file()
         del credentials[username]
         self._write_credentials_file(credentials)
@@ -38,4 +38,4 @@ class JsonCredentialsAuthenticator(Authenticator):
             json.dump(str_credentials, f, indent=4)
 
 
-auth = JsonCredentialsAuthenticator("app_data/secrets/creds.json")
+auth = Authenticator(JsonCredentialsStorage("app_data/secrets/creds.json"))
--- src/opa/core/auth.py
+++ src/opa/core/auth.py
@@ -2,39 +2,45 @@ from abc import ABC, abstractmethod
 import secrets
 from hashlib import scrypt
 from loguru import logger
+from dataclasses import dataclass
 
 
 def encrypt_pass(password: bytes):
     return scrypt(password, n=2**14, r=8, p=1, salt=b"salt")
 
 
-class Authenticator(ABC):
+class CredentialsStorage(ABC):
     @abstractmethod
     def read_hashed_password(self, username: str) -> bytes | None:
         ...
 
     @abstractmethod
-    def write_credentials(self, username: str, password: bytes):
+    def write(self, username: str, password: bytes):
         ...
 
     @abstractmethod
-    def remove_credentials(self, username: str):
+    def remove(self, username: str):
         ...
 
+
+@dataclass
+class Authenticator:
+    credentials_storage: CredentialsStorage
+
     def add_user(self, username: str, password: str) -> bool:
-        if self.read_hashed_password(username):
+        if self.credentials_storage.read_hashed_password(username):
             logger.error("User {} already exists", username)
             return False
         else:
-            self.write_credentials(username, encrypt_pass(password.encode()))
+            self.credentials_storage.write(username, encrypt_pass(password.encode()))
             logger.info("User {} successfully added", username)
             return True
 
     def remove_user(self, username: str):
-        self.remove_credentials(username)
+        self.credentials_storage.remove(username)
 
     def auth_user(self, username, password):
-        expected_password = self.read_hashed_password(username)
+        expected_password = self.credentials_storage.read_hashed_password(username)
         if expected_password is None:
             return False
         else:
