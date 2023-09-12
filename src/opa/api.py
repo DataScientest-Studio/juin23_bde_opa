cb9eb3edc539c05be0e494f440d65d58987eb63b
jherve <julien.jev.herve@gmail.com>
2023-09-12 10:47:12 +0200

    [wip] Add API securisation via basic authentication

--- src/opa/api.py
+++ src/opa/api.py
@@ -1,7 +1,12 @@
+import secrets
+import json
+import base64
+from hashlib import scrypt
 from typing import Annotated, Optional
 
 from loguru import logger
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Depends
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 from opa.core.financial_data import StockValue, StockValueKind, CompanyInfo
 from opa.storage import opa_storage
@@ -10,10 +15,39 @@ from opa.storage import opa_storage
 logger.info("API app starting up...")
 
 app = FastAPI()
+security = HTTPBasic()
+
+
+def encrypt_pass(password):
+    c = scrypt(password, n=2, r=3, p=5, salt=b"salt")
+    return base64.b64encode(c).decode("ascii")
+
+
+creds_file = "app_data/secrets/creds.json"
+
+
+def read_creds():
+    with open(creds_file) as f:
+        return json.load(f)
+
+
+def check_credentials(credentials: HTTPBasicCredentials):
+    all_users = {"bob": encrypt_pass(b"pouet")}
+    with open(creds_file, "w") as f:
+        json.dump(all_users, f)
+    expected_password = all_users.get(credentials.username)
+    if expected_password:
+        if secrets.compare_digest(
+            expected_password, encrypt_pass(credentials.password.encode())
+        ):
+            return True
+    print("Bad credentials")
+    return False
 
 
 @app.get("/")
-async def root():
+async def root(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    print("creds:", check_credentials(credentials))
     return {"message": "Hello World"}
 
 
