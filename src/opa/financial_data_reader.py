cd28480f6464e44496da6d83afd129f361f07378
jherve <julien.jev.herve@gmail.com>
2023-09-25 17:57:04 +0200

    Move declaration of tickers' list into settings

--- src/opa/financial_data_reader.py
+++ src/opa/financial_data_reader.py
@@ -3,12 +3,13 @@ from loguru import logger
 from opa.core import FinancialDataReader
 from opa.providers import opa_provider
 from opa.storage import opa_storage
+from opa.config import settings
 
 
 if __name__ == "__main__":
     logger.info("Reader app starting up...")
 
     reader = FinancialDataReader(opa_provider, opa_storage)
-    reader.run(["AAPL", "MSFT", "AMZN", "GOOG", "META"])
+    reader.run(settings.tickers_list)
 
     logger.info("Reader app done")
